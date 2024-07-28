43deac803395e38c17c424741118d3eccc41bbeb
Aaron Johnson <77075455+Aaronsupa@users.noreply.github.com>
2024-07-28 22:44:08 +0000

    updates

--- devc-react-app/src/App.tsx
+++ devc-react-app/src/App.tsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import { Box, Card, Center, Container, Divider, Flex, FormControl, FormHelperText, FormLabel, HStack, Input, Spacer, Stack, VStack, Text, Button, Link} from '@chakra-ui/react'
-import { Heading } from '@chakra-ui/react'
 import './App.css'
 import AboutMe from './components/AboutMe'
 import Information from './components/Information'
 import ContactMe from './components/ContactMe'
-import pic from "../assets/test-bkg.jpg"
-import Testimonials from './components/Testimonials'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>

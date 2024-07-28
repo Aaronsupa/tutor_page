1facd1ef43966434df68d48fdda50da14289fb46
Aaron Johnson <77075455+Aaronsupa@users.noreply.github.com>
2024-07-28 23:40:24 +0000

    updates

--- devc-react-app/vite.config.ts
+++ devc-react-app/vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], base: "./tutor_page",
+  base: "./tutor_page",
+  plugins: [react()],
   server: {
     watch: {
       usePolling: true,

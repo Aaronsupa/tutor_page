69105cb2180b9501dc7a4533bedbe314e0d9090b
Aaron Johnson <77075455+Aaronsupa@users.noreply.github.com>
2024-07-28 04:58:43 +0000

    commit

--- devc-react-app/src/components/Information.tsx
+++ devc-react-app/src/components/Information.tsx
@@ -15,9 +15,8 @@ function Information() {
     return (
         <>
         <Box w={{base: '95%', sm: '90%', md:'55%' }} h = {{base: '30%', sm: '25%', md:'25%' }}>
-          <Box bg='#FEFBF6' w='100%' h = '100%' color='black' borderWidth='2px' borderRadius='lg' borderColor='#2C4E8070' boxShadow='6px 6px #2C4E8070;' shadow='xl' display="flex" alignItems="center" overflowY="auto" flexDirection="column">
+          <Box bg='#FEFBF6' w='100%' h = '100%' color='black' borderWidth='3px' borderRadius='lg' borderColor='#474F7A80' boxShadow='6px 6px #474F7A;' shadow='xl' display="flex" alignItems="center" flexDirection="column">
             <Heading color="#2C4E80" marginBottom="2%" marginTop="2%"> Information & Scheduling</Heading>
-            <Divider orientation='horizontal' borderColor="#1F417280" borderWidth='2px' w='90%' borderRadius="10%"></Divider>
             <HStack spacing='10px' h = "90%" w = "100%">
               <VStack spacing='24px' h = "100%" w = "40%">
                 <Box w='60%' h='100%' display="flex" justifyContent="left" alignItems='center' >
@@ -39,7 +38,7 @@ function Information() {
                   </Stack>
                 </Box>
               </VStack>
-              <Divider orientation='vertical' borderColor="#F0F0F0" borderWidth='2px' h='99%'></Divider>
+              <Divider orientation='vertical' borderColor="#474F7A40" borderWidth='2px' h='80%'></Divider>
               <VStack spacing='24px' h = {{base: '100%', sm: '100%', md:'85%'}} w = "60%" display="flex" justifyContent="space-between">
                 <Box w='80%' h={{base: '80%', sm: '80%', md:'50%'}} bg='#FEFAF6' display="flex" flexDirection="column" justifyContent="space-between">
                   <Text  fontSize={{base: 'sm', sm: 'xl', md:'xl'}} alignItems="center" display="flex" as="b">Meetings</Text>

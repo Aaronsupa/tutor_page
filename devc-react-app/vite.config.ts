37de0648b1b98da9e92e95ed9993345d83534b87
Aaron Johnson <77075455+Aaronsupa@users.noreply.github.com>
2024-07-29 02:16:18 +0000

    updates

--- devc-react-app/vite.config.ts
+++ devc-react-app/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tutor_page",
+  base: "/tutor_page/",
   plugins: [react()],
   server: {
     watch: {
